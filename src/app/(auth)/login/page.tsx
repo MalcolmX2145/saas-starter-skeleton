05bccf57e039f41e6b04c0229d8656c72e118385
MalcolmX2145 <ekajjamalcolm@gmail.com>
2025-09-30 19:32:00 +0300

    Places components

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginPage from '@/components/login'
+import React from 'react'
+
+function page() {
+  return (
+    <LoginPage />
+  )
+}
+
+export default page
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupPage from '@/components/sign-up'
+import React from 'react'
+
+function page() {
+  return (
+    <SignupPage />
+  )
+}
+
+export default page
